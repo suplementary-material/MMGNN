a59144cbf020339b8ff5553cb203fc1d128ffce5
suplementary-material <biwendong1997@gmail.com>
2022-08-24 16:14:49 +0800

    'reset'

--- citation_network/run.sh
+++
@@ -1,3 +0,0 @@
-# python3 main_citation.py --data pubmed --hidden 256 --lamda 0.4 --wd1 5e-4 --num_epoch 600
-# python3 main_citation.py --data citeseer --hidden 256  --num_epoch 600
-python3 main_citation.py --data cora --test --num_epoch 400 --hidden 64
